e73eafe667e5b0fcffa671a966cf9d97fb7654da
lakhnishMonster <lakhnishmonster@gmail.com>
2024-08-26 09:14:59 -0400

    Added mod config settings

---
+++ MoreOverload/ConfigOptions.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MoreOverload { }
+
+public enum Variant
+{
+    RinkaHPDefault,
+    RinkaHPPlusOne,
+
+    hokaOverburnDefault,
+    hokaOverburnLess,
+
+    hokaHPDefault,
+    hokaHPMinusOne,
+    hokaHPMinusTwo,
+
+
+    aotaroHPDefault,
+    aotaroHPMinusOne,
+    aotaroHPMinusTwo,
+
+    aotaroAttackDefault,
+    aotaroAttackLess,
+    
+    aotaroCounterDefault,
+    aotaroCounterMore
+}
--- MoreOverload/MoreOverload.cs
+++ MoreOverload/MoreOverload.cs
@@ -6,6 +6,8 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using WildfrostHopeMod;
+using WildfrostHopeMod.Configs;
 
 namespace MoreOverload
 {
@@ -15,13 +17,125 @@ namespace MoreOverload
         {
         }
 
+
+        //-------MOD CONFIGS------\\
+        //RINKA HP CONFIG
+        [ConfigItem(Variant.RinkaHPDefault, "", "rinkaConfig")]
+        [ConfigManagerTitle("Set Rinka's HP")]
+        [ConfigManagerDesc("Requires game restart and new run for changes to take effect.")]
+        [ConfigOptions(new string[] { "1 HP (default)",
+                                      "2 HP" },
+        new object[]
+        {
+            new Variant[]
+            {
+                Variant.RinkaHPDefault,
+                Variant.RinkaHPPlusOne
+            }
+        }
+        )]
+        public Variant rinkaHPVariant; 
+        
+        //HOKA OVERBURN CONFIG
+        [ConfigItem(Variant.hokaOverburnDefault, "", "hokaConfig")]
+        [ConfigManagerTitle("Set Hoka's Overburn Amount")]
+        [ConfigManagerDesc("Requires game restart and new run for changes to take effect.")]
+        [ConfigOptions(new string[] { "2 Overburn (default)",
+                                      "1 Overburn" },
+        new object[]
+        {
+            new Variant[]
+            {
+                Variant.hokaOverburnDefault,
+                Variant.hokaOverburnLess
+            }
+        }
+        )]
+        public Variant hokaOverburnVariant;
+
+        //HOKA HP CONFIG
+        private int hokaHPStat;
+
+        [ConfigItem(Variant.hokaHPDefault, "", "hokaHPConfigs")]
+        [ConfigManagerTitle("Set Hoka's HP")]
+        [ConfigManagerDesc("Requires game restart and new run for changes to take effect.")]
+        [ConfigOptions(new string[] { "6 HP (default)",
+                                      "5 HP",
+                                      "4 HP" },
+        new object[]
+        {
+            new Variant[]
+            {
+                Variant.hokaHPDefault,
+                Variant.hokaHPMinusOne,
+                Variant.hokaHPMinusTwo,
+            }
+        }
+        )]
+        public Variant hokaHPVariant;
+
+        //AOTARO HP CONFIG
+        private int aotaroHPStat;
+        private int aotaroCounterStat;
+        private int aotaroAttackStat;
+
+        [ConfigItem(Variant.aotaroHPDefault, "", "aotaroHPConfigs")]
+        [ConfigManagerTitle("Set Aotaro's HP")]
+        [ConfigManagerDesc("Requires game restart and new run for changes to take effect.")]
+        [ConfigOptions(new string[] { "8 HP (default)",
+                                      "7 HP",
+                                      "6 HP" },
+        new object[]
+        {
+            new Variant[]
+            {
+                Variant.aotaroHPDefault,
+                Variant.aotaroHPMinusOne,
+                Variant.aotaroHPMinusTwo,
+            }
+        }
+        )]
+        public Variant aotaroHPVariant;
+
+        //AOTARO ATTACK
+        [ConfigItem(Variant.aotaroAttackDefault, "", "aotaroAttackConfigs")]
+        [ConfigManagerTitle("Set Aotaro's Attack")]
+        [ConfigManagerDesc("Requires game restart and new run for changes to take effect.")]
+        [ConfigOptions(new string[] { "3 Attack (default)",
+                                      "2 Attack" },
+        new object[]
+        {
+            new Variant[]
+            {
+                Variant.aotaroAttackDefault,
+                Variant.aotaroAttackLess
+            }
+        }
+        )]
+        public Variant aotaroAttackVariant;
+
+        //AOTARO TURN TIMER
+        [ConfigItem(Variant.aotaroCounterDefault, "", "aotaroCounterConfigs")]
+        [ConfigManagerTitle("Set Aotaro's Counter")]
+        [ConfigManagerDesc("Requires game restart and new run for changes to take effect.")]
+        [ConfigOptions(new string[] { "4 Turn Timer (default)",
+                                      "5 Turn Timer " },
+        new object[]
+        {
+            new Variant[]
+            {
+                Variant.aotaroCounterDefault,
+                Variant.aotaroCounterMore
+            }
+        }
+        )]
+        public Variant aotaroCounterVariant;
+
+
         //MOD INFO
         public override string GUID => "lakhnish_monster.wildfrost.moreoverload";
-
-        public override string[] Depends => new string[0];
-
+        public override string[] Depends => new string[1] { "hope.wildfrost.configs" };
         public override string Title => "Even More Overburn";
-
         public override string Description => "Adds three new Overburn units to the Shademancer clan and 1 new pet!";
 
         //CARDS SETUP
@@ -60,7 +174,6 @@ namespace MoreOverload
         {
             statusEffects = new List<StatusEffectDataBuilder>();
             //STATUS EFFECTS HERE
-            
             statusEffects.Add(
                 StatusCopy("When Snow Applied To Anything Gain Equal Attack To Self", "When Overload Applied To Anything Gain Equal Attack To Self")
                 .WithText("Whenever anything is <keyword=overload>'d, gain equal <keyword=attack>")
@@ -69,37 +182,10 @@ namespace MoreOverload
                     ((StatusEffectApplyXWhenYAppliedTo)data).whenAppliedTypes = new string[1] { "overload" };
                 })
                 );
-            
 
             cards = new List<CardDataBuilder>();
             //CARDS HERE
 
-            //----------SHADEMANCER COMPANIONS-------\\
-            //THIS NEEDS SOMETHING MORE??? Aotaro is always a clear pick over this.
-            //Also need to keep in mind what happens in the chance there are no overburn units.
-            //This looks like old school Dimona too, which is probably why it feels bad.
-            //Need to also make sure it's not too close to the pet or I could just scrap it all together.
-            /*
-            cards.Add(
-                new CardDataBuilder(this).CreateUnit("Soen", "Soen") //Twin Flames: 双炎
-                    .SetSprites("", "Zula_BG.png")
-                    .SetStats(6, 0, 3)
-                    .AddPool("MagicUnitPool")
-                    .SetAttackEffect(SStack("Demonize", 1), SStack("Overload", 1))
-                    .SetStartWithEffect(SStack("MultiHit",1)) //maybe barrage instead. Demonize + overload on aotaro is super strong
-                    );
-            */  
-            //I love this unit, but it's also really strong. Maybe needs a 5 turn timer? I like the HP stat. 1 less attack might also be better?
-            cards.Add(
-                new CardDataBuilder(this).CreateUnit("Aotaro", "Aotaro")//Blue Axe Man: Aotaro 青男
-                    .SetSprites("", "Aotaro_BG.png")
-                    .SetStats(8, 3, 4)
-                    .AddPool("MagicUnitPool")
-                    .SetStartWithEffect(SStack("On Hit Equal Overload To Target", 1))
-                    );
-
-            //Nice alternative to spike for the 1st 3 fights, can help get more multikills in fights 2 & 3 compared to Spike.
-            //Doesn't fair too well afterwards and I don't see it being a carry for end game like Booshu, but I like it.
             cards.Add(
                 new CardDataBuilder(this).CreateUnit("Azuryuu", "Azuryuu") //Azul Dragon: アズル竜
                     .SetSprites("", "Azuryuu_BG.png")
@@ -108,37 +194,130 @@ namespace MoreOverload
                     .AddPool("MagicUnitPool")
                     .SetAttackEffect(SStack("Overload", 2))
                     );
-            //1 Overload seems too little, 2 seems good but when lumin'd, it scales too fast w/ Hoka.
-            //Needs more testing
+
+            /*
             cards.Add(
-                new CardDataBuilder(this).CreateUnit("Hoka", "Hoka") //Arson: 放火
+                new CardDataBuilder(this).CreateUnit("Soen", "Soen") //Twin Flames: 双炎
                     .SetSprites("", "Zula_BG.png")
-                    .SetStats(6, null, 4) //6hp tp survive a 5 damage Grok hit
+                    .SetStats(6, 0, 3)
                     .AddPool("MagicUnitPool")
-                    .SetStartWithEffect(SStack("On Turn Apply Overload To Enemies", 2))
+                    .SetAttackEffect(SStack("Demonize", 1), SStack("Overload", 1))
+                    .SetStartWithEffect(SStack("MultiHit", 1)) //maybe barrage instead. Demonize + overload on aotaro is super strong
                     );
-            //Yuki alternate, but scales too fast with Hoka, probably because there isn't an overload guard like there is for Snow.
-            //Needs more testing
-            cards.Add(
-                new CardDataBuilder(this).CreateUnit("Rinka", "Rinka") //Will-o'-the Wisp: 燐火
-                    .SetSprites("", "Zula_BG.png")
-                    .SetStats(1, 1, 4)
-                    .AddPool("MagicUnitPool")
-                    .SubscribeToAfterAllBuildEvent(delegate (CardData data)
-                    {
-                        data.startWithEffects = new CardData.StatusEffectStacks[1]
-                        {
-                           SStack("When Overload Applied To Anything Gain Equal Attack To Self",1)
-                        };
-                    }));
-          
+            */
+
+
+            //BASE AOTARO
+            CardDataBuilder aotaroCardBuilder = new CardDataBuilder(this)
+            .CreateUnit("Aotaro", "Aotaro") //Blue Axe Man: Aotaro 青男
+            .SetSprites("", "Aotaro_BG.png")
+            .SetStartWithEffect(SStack("On Hit Equal Overload To Target", 1))
+            .AddPool("MagicUnitPool");
+
+            //AOTARO HP
+            switch (aotaroHPVariant)
+            {
+                case Variant.aotaroHPDefault:
+                    aotaroHPStat = 8;
+                    break;
+                case Variant.aotaroHPMinusOne:
+                    aotaroHPStat = 7;
+                    break;
+                case Variant.aotaroHPMinusTwo:
+                    aotaroHPStat = 6;
+                    break;
+            }
+            //AOTARO ATTACK
+            switch (aotaroAttackVariant)
+            {
+                case Variant.aotaroAttackDefault:
+                    aotaroAttackStat = 3;
+                    break;
+
+                case Variant.aotaroAttackLess:
+                    aotaroAttackStat = 2;
+                    break;
+            }
+            //AOTARO COUNTER
+            switch (aotaroCounterVariant)
+            {
+                case Variant.aotaroCounterDefault:
+                    aotaroCounterStat = 4;
+                    break;
+
+                case Variant.aotaroCounterMore:
+                    aotaroCounterStat = 5;
+                    break;
+
+            }
+            aotaroCardBuilder.SetStats(aotaroHPStat, aotaroAttackStat, aotaroCounterStat);
+            cards.Add(aotaroCardBuilder);
+
+            //BASE HOKA
+            CardDataBuilder hokaCardBuilder = new CardDataBuilder(this)
+            .CreateUnit("Hoka", "Hoka") //Arson: 放火
+            .SetSprites("", "Zula_BG.png")
+            //.SetStats(6, null, 4) //6hp tp survive a 5 damage Grok hit
+            .AddPool("MagicUnitPool");
+            switch (hokaOverburnVariant)
+            {
+                case Variant.hokaOverburnDefault:
+                    hokaCardBuilder.SetStartWithEffect(SStack("On Turn Apply Overload To Enemies", 2));
+                    break;
+
+                case Variant.hokaOverburnLess:
+                    hokaCardBuilder.SetStartWithEffect(SStack("On Turn Apply Overload To Enemies", 1));
+                    break;
+
+            }
+            switch (hokaHPVariant)
+            {
+                case Variant.hokaHPDefault:
+                    hokaHPStat = 6;
+                    break;
+                case Variant.hokaHPMinusOne:
+                    hokaHPStat = 5;
+                    break;
+                case Variant.hokaHPMinusTwo:
+                    hokaHPStat = 4;
+                    break;
+            }
+            hokaCardBuilder.SetStats(hokaHPStat, null, 4);
+            cards.Add(hokaCardBuilder);
+ 
+            //BASE RINKA
+            CardDataBuilder rinkaCardBuilder = new CardDataBuilder(this)
+            .CreateUnit("Rinka", "Rinka") //Will-o'-the Wisp: 燐火
+            .SetSprites("", "Zula_BG.png")
+            .SubscribeToAfterAllBuildEvent(delegate (CardData data)
+            {
+                data.startWithEffects = new CardData.StatusEffectStacks[1]
+                {
+                    SStack("When Overload Applied To Anything Gain Equal Attack To Self",1)
+                };
+            });
+            switch (rinkaHPVariant)
+            {
+                case Variant.RinkaHPDefault:
+                    rinkaCardBuilder.SetStats(1, 1, 4);
+                    break;
+
+                case Variant.RinkaHPPlusOne:
+                    rinkaCardBuilder.SetStats(2, 1, 4);
+                    break;
+
+            }
+            cards.Add(rinkaCardBuilder);
+
             preLoaded = true;
         }
-        
+
 
         protected override void Load()
         {
             if (!preLoaded) { CreateModAssets(); }
+
+            ConfigManager.OnModLoaded += HandleModLoaded;
             base.Load();
         }
 
@@ -146,6 +325,24 @@ namespace MoreOverload
         {
             base.Unload();
         }
+
+        private void HandleModLoaded(WildfrostMod mod)
+        {
+            if (!(mod.GUID != "hope.wildfrost.configs"))
+            {
+                ConfigSection configSection = ConfigManager.GetConfigSection(this);
+                if (configSection != null)
+                {
+                    configSection.OnConfigChanged += HandleConfigChange;
+                }
+            }
+        }
+
+        private void HandleConfigChange(ConfigItem item, object value)
+        {
+            Debug.Log("[MoreOverload] config changed!!");
+        }
+
 
         public override List<T> AddAssets<T, Y>()
         {
